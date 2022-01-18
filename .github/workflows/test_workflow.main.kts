07051c79218a401819f99077aad754dfc15058e8
Piotr Krzeminski <git@krzeminski.it>
2022-01-18 09:20:18 +0100

    Fix script

--- .github/workflows/test_workflow.main.kts
+++ .github/workflows/test_workflow.main.kts
@@ -1,5 +1,13 @@
 #!/usr/bin/env kotlin
 
+@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.1.0")
+
+import it.krzeminski.githubactions.actions.Checkout
+import it.krzeminski.githubactions.domain.RunnerType.UbuntuLatest
+import it.krzeminski.githubactions.domain.Trigger.Push
+import it.krzeminski.githubactions.dsl.workflow
+import it.krzeminski.githubactions.yaml.toYaml
+
 val workflow = workflow(
     name = "Test workflow",
     on = listOf(Push),
