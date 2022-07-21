350cd5cb1467ed42c0e1ecb7a0e6e25244ca33c3
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2022-07-21 15:12:40 +0200

    Create some-script.kts

---
+++ some-script.kts
@@ -0,0 +1,17 @@
+#!/usr/bin/env kotlin
+@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.22.0")
+@file:DependsOn(
+  "org.eclipse.jgit:org.eclipse.jgit:4.6.0.201612231935-r"
+)
+
+@file : DependsOn
+  (
+  "org.jetbrains.lets-plot:lets-plot-kotlin-jvm:3.0.2"
+
+)
+
+// ...
+
+fun main() {
+    println("Hello world")
+}
