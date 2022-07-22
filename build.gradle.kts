7fa405497da6b501d9a8df5e11b267577cfb0217
Piotr Krzeminski <testingrenovate@krzeminski.it>
2022-07-22 07:28:27 +0200

    Update dependency org.eclipse.jgit:org.eclipse.jgit to v6

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ version = "1.3.1-SNAPSHOT"
 
 #!/usr/bin/env kotlin
 @file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.22.0" )
-@file:DependsOn("org.eclipse.jgit:org.eclipse.jgit:4.6.0.201612231935-r")
+@file:DependsOn("org.eclipse.jgit:org.eclipse.jgit:6.2.0.202206071550-r")
 
 // ...
 
