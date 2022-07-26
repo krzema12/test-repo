791821f7420131a0f8bcce3bbce835dba058f50d
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2022-07-26 14:00:56 +0200

    Create some-file-4.main.kts

---
+++ some-file-4.main.kts
@@ -0,0 +1,20 @@
+#!/usr/bin/env kotlin
+@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:[0.2.0,)")
+@file:DependsOn(
+  "org.eclipse.jgit:org.eclipse.jgit:[4.6.0.201612231935-r]"
+)
+
+@file : DependsOn
+  (
+  "org.jetbrains.lets-plot:lets-plot-kotlin-jvm:3.0.2"
+
+)
+@file : DependsOn
+  (
+  "org.jetbrains.lets-plot:lets-plot-kotlin-jvm:(,3.0.0]"
+
+)
+
+// ...
+
+println("Hello world")
