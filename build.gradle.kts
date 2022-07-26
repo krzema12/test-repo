2f6c8159bbe2a3be192f4f0874f2450306ab0b07
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2022-07-26 14:14:34 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,1 @@
 version = "1.3.1-SNAPSHOT"
-
-#!/usr/bin/env kotlin
-@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.22.0" )
-@file:DependsOn("org.eclipse.jgit:org.eclipse.jgit:4.6.0.201612231935-r")
-
-// ...
-
-println("Hello world")
