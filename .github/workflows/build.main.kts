8b2bba87cb6a5637bde40052dcdb128321ad5089
Piotr Krzeminski <git@krzeminski.it>
2025-01-23 13:25:26 +0100

    Try with 0.15.0

--- .github/workflows/build.main.kts
+++ .github/workflows/build.main.kts
@@ -1,18 +1,20 @@
 #!/usr/bin/env kotlin
 
-@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.30.0")
+@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.15.0")
 
 import it.krzeminski.githubactions.actions.actions.CheckoutV3
 import it.krzeminski.githubactions.domain.RunnerType.UbuntuLatest
 import it.krzeminski.githubactions.domain.triggers.Push
 import it.krzeminski.githubactions.dsl.workflow
 import it.krzeminski.githubactions.yaml.writeToFile
+import java.nio.file.Paths
 
 
 workflow(
     name = "Test workflow",
     on = listOf(Push()),
-    sourceFile = __FILE__.toPath(),
+    sourceFile = Paths.get(".github/workflows/build.main.kts"),
+    targetFile = Paths.get(".github/workflows/build.yaml")
 ) {
     job(id = "test_job", runsOn = UbuntuLatest) {
         uses(name = "Check out", action = CheckoutV3())
