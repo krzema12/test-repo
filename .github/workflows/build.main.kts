038168bb1825fe7ea7c08a949946edb1d80facaf
Piotr Krzeminski <git@krzeminski.it>
2025-01-23 13:29:10 +0100

    Try 0.4.0

--- .github/workflows/build.main.kts
+++ .github/workflows/build.main.kts
@@ -1,10 +1,10 @@
 #!/usr/bin/env kotlin
 
-@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.15.0")
+@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.4.0")
 
-import it.krzeminski.githubactions.actions.actions.CheckoutV3
+import it.krzeminski.githubactions.actions.actions.CheckoutV2
 import it.krzeminski.githubactions.domain.RunnerType.UbuntuLatest
-import it.krzeminski.githubactions.domain.triggers.Push
+import it.krzeminski.githubactions.domain.Trigger.Push
 import it.krzeminski.githubactions.dsl.workflow
 import it.krzeminski.githubactions.yaml.writeToFile
 import java.nio.file.Paths
@@ -12,12 +12,12 @@ import java.nio.file.Paths
 
 workflow(
     name = "Test workflow",
-    on = listOf(Push()),
+    on = listOf(Push),
     sourceFile = Paths.get(".github/workflows/build.main.kts"),
     targetFile = Paths.get(".github/workflows/build.yaml")
 ) {
-    job(id = "test_job", runsOn = UbuntuLatest) {
-        uses(name = "Check out", action = CheckoutV3())
+    job(name = "test_job", runsOn = UbuntuLatest) {
+        uses(name = "Check out", action = CheckoutV2())
         run(name = "Print greeting", command = "echo 'Hello world!'")
     }
 }.writeToFile()
